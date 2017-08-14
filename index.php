b682c5c33cf6b8bd66278e5579c69205a3caeee4
jkarlos1 <30414679+jkarlos1@users.noreply.github.com>
2017-08-14 15:09:33 -0500

    Subiendo los archivos base
    Todos los archivos con la conexiòn a mlab.

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   <meta charset="UTF-8">
+   <title>Conectando con la base de Datos</title>
+   <style type="text/css">
+      body{
+         background-color: gray;
+      }
+      form{
+         background-color: white;
+          font-family: Consolas;
+      }
+       h1{
+           font-family: "OCR-B 10 BT";
+           color: white;
+       }
+       h2{
+           font-family: "OCR-B 10 BT";
+       }
+       div{
+           color: white;
+       }
+       button{
+           font-family: "Consolas";
+           font-size: 20px;
+           border-radius: 10px 10px 10px 10px;
+       }
+       input{
+           border-radius: 5px 5px 5px 5px;
+       }
+       .footer{
+           font-family: "Footlight MT Light";
+           color: silver;
+           position: fixed;
+           bottom: 1%;
+           text-align: center;
+           width: 100%;
+
+       }
+      
+   </style>
+</head>
+<body>
+<h1>PHP en Heroku <?php echo ('Salida en PHP');?> </h1>
+<form action ="guardar.php" method="post">
+    <fieldset>
+        <center><h2>Datos</h2></center><br>
+        <label for="llave">Llave </label>
+        <input id="llave" name="llave" type="text"  required focus>
+
+        <label for="descripcion">Descripción </label>
+        <input id="descripcion" name="descripcion" type="text"  required focus>
+
+        <center><button type="submit">Guardar</button>
+    </fieldset>
+</form>
+<div>
+    Salida
+    <?php
+    //$llave = $_POST["llave"]; //Cuando enviemos
+    //$descripcion = $_POST["descripcion"]; //Cuando enviemos
+    require 'vendor/autoload.php';
+    $uname = "dbgamemax";
+    $pass = "dbgamemax123";
+    $dbName = "dbgame";
+    $uri = "mongodb://".$uname.":".$pass."@ds127983.mlab.com:27983/dbgame";
+    $collection = "foo";
+
+    echo ($uri);
+    $conn = new MongoClient($uri);
+
+    echo ('Nuestra base de datos en mLAB:');
+    echo $conn; // para asegurarse que la conexión esté bien.
+    echo ('<br>');
+
+
+
+    $db = $conn->selectDB($dbName);
+    //echo $db;
+
+    $col = new MongoCollection($db, $collection);
+    $cur = $col->find();
+
+    $result = array();
+    foreach ($cur as $doc) {
+        $tmp = array();
+        $tmp["a"] = $doc["a"];
+        array_push($result, $tmp);
+    }
+
+    echo json_encode($result);
+    //header("Location:index.php"); //Solo lo activaremos en casos de pruebas.
+    ?>
+</div>
+<footer>
+    <div class="footer">
+    <h4>Sistemas Distribuidos - UNAMBA 2017</h4>
+    </div>
+</footer>
+</body>
+</html>
